461ad0acb71488ddde8d6dc9c8653fb8bbd42ee8
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-17 12:37:35 +0200

    Initial Commit

---
+++ DiscardableResult/DiscardableResultApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscardableResultApp.swift
+//  DiscardableResult
+//
+//  Created by Ramill Ibragimov on 17.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct DiscardableResultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
